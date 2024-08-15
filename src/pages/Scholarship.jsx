695b9cdbfc82bf22bebe6be1798bced2bd8cbd84
Lerian Febriana <kanglerian@gmail.com>
2024-08-15 21:18:10 +0700

    hapus di scholarship

--- src/pages/Scholarship.jsx
+++ src/pages/Scholarship.jsx
@@ -46,7 +46,7 @@ const Scholarship = () => {
         let foto = fileuploaded.find((file) => { return file.namefile == "foto" });
         let akta = fileuploaded.find((file) => { return file.namefile == "akta-kelahiran" });
         let keluarga = fileuploaded.find((file) => { return file.namefile == "kartu-keluarga" });
-        if (start && applicant.name && applicant.religion && applicant.school && applicant.year && applicant.place_of_birth && applicant.date_of_birth && applicant.gender && applicant.address && applicant.email && applicant.phone && applicant.program && applicant.income_parent && applicant.father.name && applicant.father.date_of_birth && applicant.father.education && applicant.father.address && applicant.father.job && applicant.mother.name && applicant.mother.date_of_birth && applicant.mother.education && applicant.mother.address && applicant.mother.job && foto && akta && keluarga) {
+        if (start && applicant.name && applicant.religion && applicant.school && applicant.year && applicant.place_of_birth && applicant.date_of_birth && applicant.gender && applicant.address && applicant.email && applicant.phone && applicant.program && applicant.income_parent && applicant.father.name && applicant.father.date_of_birth && applicant.father.education && applicant.father.address && applicant.father.job && applicant.mother.name && applicant.mother.date_of_birth && applicant.mother.education && applicant.mother.address && applicant.mother.job) {
           console.log('lengkap');
         } else {
           navigate('/dashboard');
